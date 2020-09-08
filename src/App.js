0b96236bdf346e4e17a844fffd7586e10c6a2fd4
jlight99 <ellenhuang523@gmail.com>
2020-09-08 00:03:02 -0400

    add routing trick, test homepage

--- src/App.js
+++ src/App.js
@@ -10,18 +10,20 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FoodSearchPage from './FoodSearchPage';
 import { getUserFromLocalStorage } from './SignIn';
+import TestHome from './TestHome';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
+          <Route exact path="/" component={TestHome} />
           <Route path="/signin" component={SignIn} />
-          {!getUserFromLocalStorage() &&
+          {/* {!getUserFromLocalStorage() &&
             <span>
               <Redirect to="/signin" />
             </span>
-          }
+          } */}
           {getUserFromLocalStorage() &&
             <span>
               <Route path="/days" component={Days} />
---
+++ src/TestHome.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function TestHome() {
+    return (
+        <div>Refactored Spoon!</div>
+    );
+}
