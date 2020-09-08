99b5fc99910b4020d711bd9eb4d6e1674ce53298
jlight99 <ellenhuang523@gmail.com>
2020-09-07 23:12:34 -0400

    Updates

---
+++ precache-manifest.940471dee1ee5afac5d0ed3f5cf9817c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6d66e0b3cdf53ee46669027755f390",
+    "url": "/refactored-spoon/index.html"
+  },
+  {
+    "revision": "610431417383383e010e",
+    "url": "/refactored-spoon/static/css/2.6d52cc7f.chunk.css"
+  },
+  {
+    "revision": "4537b4cd059262104985",
+    "url": "/refactored-spoon/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "610431417383383e010e",
+    "url": "/refactored-spoon/static/js/2.f322c126.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/refactored-spoon/static/js/2.f322c126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4537b4cd059262104985",
+    "url": "/refactored-spoon/static/js/main.9fe5f15c.chunk.js"
+  },
+  {
+    "revision": "4eafd143691d924f3961",
+    "url": "/refactored-spoon/static/js/runtime-main.0ed87baa.js"
+  }
+]);
